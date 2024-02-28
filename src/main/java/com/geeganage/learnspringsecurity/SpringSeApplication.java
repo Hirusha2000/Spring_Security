bfe60a92fbe8c80d63efa57156d05471b9bb2fe8
Hirusha2000 <2000hirushaudara@gmail.com>
2024-02-28 11:06:33 +0530

    first commit

---
+++ src/main/java/com/geeganage/learnspringsecurity/SpringSeApplication.java
@@ -0,0 +1,13 @@
+package com.geeganage.learnspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSeApplication.class, args);
+    }
+
+}
