0ff2be03f769df8b9ff8e81042b3d51cd12fdcfd
Hirusha2000 <2000hirushaudara@gmail.com>
2024-02-28 11:38:31 +0530

    security fundamentals

--- src/main/java/com/geeganage/learnspringsecurity/HelloWorldResource.java
+++ src/main/java/com/geeganage/learnspringsecurity/HelloWorldResource.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldResource {
-    @GetMapping("/hello=world")
+    @GetMapping("/hello-world")
     public String helloWorld(){
-        return "Hello World";
+        return "Hello World v1";
     }
 }
